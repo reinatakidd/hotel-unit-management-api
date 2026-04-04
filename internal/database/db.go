19ea72fcdca50319c312df78b48ecb5ec06baaa8
KidKat <gunawan.kuncoro@binus.ac.id>
2026-04-04 18:20:53 +0700

    feat: add docker with environment-based database config
    - Create Dockerfile and docker-compose with MySQL services
    - Update DB connection to use environment variables

--- internal/database/db.go
+++ internal/database/db.go
@@ -2,15 +2,34 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
+func getEnv(key, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return fallback
+}
+
 func Connect() {
-	dsn := "root:@tcp(localhost:3306)/bobobox-db?parseTime=true"
+	_ = godotenv.Load()
+
+	host := getEnv("DB_HOST", "localhost")
+	port := getEnv("DB_PORT", "3306")
+	user := getEnv("DB_USER", "root")
+	password := getEnv("DB_PASSWORD", "")
+	dbname := getEnv("DB_NAME", "bobobox-db")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		user, password, host, port, dbname)
 
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
